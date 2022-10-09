2491772040fcf9e6f2cb419a4c480c9438d1290a
Ulisses Gomes <ulissesdeveloper82@gmail.com>
2022-10-09 16:01:19 -0300

    feat: Criando os métodos depositar,sacar,transferir,cancelarConta.

--- desafio-controle-financeiro/src/main/java/entities/ContaCorrente.java
+++ desafio-controle-financeiro/src/main/java/entities/ContaCorrente.java
@@ -2,12 +2,15 @@ package entities;
 
 import java.util.Date;
 
+import entities.exceptions.DomainException;
+
 public class ContaCorrente {
 	private Integer numeroConta;
 	private Integer numeroAgencia;
 	private String nomeCliente;
 	private Date dataNascimento;
 	private Double saldo;
+	private Boolean cancelada = false;
 
 	public ContaCorrente() {
 	}
@@ -39,6 +42,49 @@ public class ContaCorrente {
 
 	public Double getSaldo() {
 		return saldo;
+	}
+
+	public Boolean getCancelada() {
+		return cancelada;
+	}
+
+	public void depositar(Double valor) throws DomainException {
+		if (valor <= 0.0) {
+			throw new DomainException("O valor de depósito deve ser superior a zero.");
+		} else if (cancelada) {
+			throw new DomainException("Conta cancelada.");
+		}
+		saldo += valor;
+	}
+
+	public void sacar(Double valor) throws DomainException {
+		if (valor <= 0.0) {
+			throw new DomainException("O valor de saque deve ser superior a zero.");
+		} else if (valor > saldo) {
+			throw new DomainException("Saldo insuficiente para esta transação.");
+		} else if (cancelada) {
+			throw new DomainException("Conta cancelada.");
+		}
+		saldo -= valor;
+	}
+
+	public void transferir(ContaCorrente contaDestino, Double valor) throws DomainException {
+		if (contaDestino.equals(null)) {
+			throw new DomainException("Deve ser informado uma conta para transferir o valor.");
+		} else if (cancelada) {
+			throw new DomainException("Conta cancelada.");
+		}
+		sacar(valor);
+		contaDestino.depositar(valor);
+	}
+
+	public void cancelarConta(String justificativa) throws DomainException {
+		if (justificativa == null || justificativa.isBlank()) {
+			throw new DomainException("Informe uma justificativa para o cancelamento.");
+		} else if (cancelada) {
+			throw new DomainException("Conta cancelada.");
+		}
+		cancelada = true;
 	}
 
 }
