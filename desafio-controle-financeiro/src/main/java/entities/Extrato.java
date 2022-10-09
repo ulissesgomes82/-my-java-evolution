805380901acd8c42cc238632c705f161238f34ba
Ulisses Gomes <ulissesdeveloper82@gmail.com>
2022-10-09 17:12:30 -0300

    feat: Criando a classe Extrato

---
+++ desafio-controle-financeiro/src/main/java/entities/Extrato.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Extrato {
+	private LocalDate dataOperacao;
+	private Double valor;
+	private operacaoOperacao operacao;
+
+	public Extrato(Double valor, operacaoOperacao operacao) {
+		dataOperacao = LocalDate.now();
+		this.valor = valor;
+		this.operacao = operacao;
+	}
+
+	public LocalDate getDataOperacao() {
+		return dataOperacao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public operacaoOperacao getoperacao() {
+		return operacao;
+	}
+
+	public enum operacaoOperacao {
+		DEPOSITO, SAQUE, TRANSFERÊNCIA
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Data Operação: " 
+		+ dataOperacao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")) 
+		+ "\nOperação: " + operacao
+		+ "\nValor: " + String.format("%.2f%n", valor));
+		
+		return sb.toString();
+	}
+}
