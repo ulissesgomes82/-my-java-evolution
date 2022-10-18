6ab990f1ec19f03617a6c5077dda73bbd40be0ed
Ulisses Gomes <ulissesdeveloper82@gmail.com>
2022-10-18 19:56:48 -0300

    feat: Criando o método gerarRelatorio,formatCPF,formatRG,formatCelular,formatarValor,formataDataHora,formatCNPJ

--- desafio-agua-luz-atracao/src/main/java/services/Cadastro.java
+++ desafio-agua-luz-atracao/src/main/java/services/Cadastro.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
-import entities.Contratante;
+import javax.swing.text.MaskFormatter;
+
 import entities.ContratoInstalacao;
-import entities.Endereco;
-import entities.enums.TipoNotificacao;
 import entities.enums.TipoServico;
-import entities.enums.TipoUnidadeFederativa;
 
 public class Cadastro {
 	public static void salvarCadastro(String nomeArquivo, ContratoInstalacao contrato) throws IOException {
@@ -57,4 +60,93 @@ public class Cadastro {
 				StandardOpenOption.APPEND);
 	}
 
+//	public static String buscarCadastro(String path, String protocolo) throws IOException {
+//		return Files.lines(Path.of(path)).filter(c->c.contains(protocolo)).toList().get(0);
+//	}
+
+	public static void gerarRelatorio(String path, String protocoloBusca) throws IOException, ParseException {
+		String cadastro = Files.lines(Path.of(path)).filter(c -> c.contains(protocoloBusca)).toList().get(0);
+
+		String cpf = formatCPF(cadastro.substring(0, 11).trim());
+		String rg = cadastro.substring(11, 21).trim();
+		String nome = cadastro.substring(21, 51).trim();
+		String celular = formatarCelular(cadastro.substring(51, 62).trim());
+		String logradouro = cadastro.substring(62, 82).trim();
+		Integer numero = Integer.parseInt(cadastro.substring(82, 88).trim());
+		String complemento = cadastro.substring(88, 98).trim();
+		String bairro = cadastro.substring(98, 113).trim();
+		String cidade = cadastro.substring(113, 143).trim();
+		String uf = cadastro.substring(143, 145).trim();
+		String cep = formatCep(cadastro.substring(145, 153).trim());
+		String pais = cadastro.substring(153, 155).trim();
+		String protocolo = cadastro.substring(155, 165).trim();
+		String dataHora = formatarDataHora(cadastro.substring(165, 177).trim());
+		TipoServico servico = TipoServico.getServico(cadastro.substring(177, 178));
+		String valor = formatarValor(cadastro.substring(179, 186).trim());
+		String tipoNotificacaoo = cadastro.substring(186).trim();
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(String.format(
+				"Senhor(a) %s cpf numero %s, informamos que conforme contrato com %n"
+						+ "protocolo de número %s está agendado para a data\\hora %sh a instalação do serviço %n"
+						+ "de %s com taxa de valor %s em sua residência localizada no endereço abaixo%n",
+				nome, cpf, protocolo, dataHora, servico, valor));
+
+		sb.append(String.format(
+				"\u2022 Logradouro: %s%n" + "\u2022 Complememto: %s%n" + "\u2022 Bairro: %s%n"
+						+ "\u2022 Cidade: %s/%s%n" + "\u2022 Cep: %s",
+				logradouro, complemento, bairro, cidade, uf, cep));
+		System.out.println(sb.toString());
+	}
+
+	public static String formatCPF(String cpf) {
+		return Pattern.compile("(\\d{3})(\\d{3})(\\d{3})(\\d{2})").matcher(cpf).replaceAll("$1.$2.$3-$4");
+	}
+
+	public static String formatRG(String rg) {
+		if (rg.length() == 9) {
+			return Pattern.compile("(\\d{2})(\\d{3})(\\d{3})(\\d{1})").matcher(rg).replaceAll("$1.$2.$3-$4");
+		} else {
+			return Pattern.compile("(\\d{3})(\\d{3})(\\d{3})(\\d{2})").matcher(rg).replaceAll("$1.$2.$3-$4");
+		}
+	}
+
+	public static String formatCNPJ(String cnpj) {
+		return Pattern.compile("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})").matcher("15587987000191")
+				.replaceAll("$1.$2.$3/$4-$5");
+	}
+
+	public static String formatCep(String cep) {
+		return Pattern.compile("(\\d{3})(\\d{3})(\\d{2})").matcher(cep).replaceAll("$1.$2-$3");
+	}
+
+	public static String formatarDataHora(String dataRelatorio) {
+		DateTimeFormatter formatarStringParaData = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+		DateTimeFormatter formatarData = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		return LocalDateTime.parse(dataRelatorio, formatarStringParaData).format(formatarData);
+	}
+
+	public static String formatarValor(String valor) {
+		Double valorFormatado = Double.parseDouble(valor) / 100;
+		return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(valorFormatado);
+	}
+
+	public static String formatarCelular(String celular) throws ParseException {
+		MaskFormatter mf = new MaskFormatter("(##)#####-####");
+		mf.setValueContainsLiteralCharacters(false);
+		return mf.valueToString(celular);
+	}
+
+//	public static void main(String[] args) throws IOException, ParseException {
+//
+//		Endereco endereco = new Endereco("Rua Sebastião Firmino",123, "AP 210 BL CENTAURO", "São Sebastião",
+//				"São Raimundo Nonato", TipoUnidadeFederativa.valueOf("sp".toUpperCase()), "07.210.715");
+//		Contratante contratante = new Contratante("007.324.223.21", "33765-5",
+//				"Raimundo Nonato Loureiro Castelo Branco", "(11) 99768-1515", endereco);
+//		ContratoInstalacao contrato = new ContratoInstalacao(1984365, TipoServico.AGUA, TipoNotificacao.SMS,
+//				contratante);
+//		Cadastro.salvarCadastro("agua-luz-output", contrato);
+//		Cadastro.gerarRelatorio("C:\\estudo1\\mjv-java-school\\agua-luz-output", "1984365");
+//	}
 }
